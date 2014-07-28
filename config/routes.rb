9971d86933743f6d7d89362185f3182eb1f9226a
Liane Allen <liantics.antics@gmail.com>
2014-07-28 11:11:41 -0400

    Let users create posts, Let users view all posts

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ require "monban/constraints/signed_out"
 
 Rails.application.routes.draw do
   constraints Monban::Constraints::SignedIn.new do
-    root "posts#index", as: :dashboard
+    root "posts#index"
     resource :session, only: [:destroy]
+    resource :posts, only: [:new, :create, :index]
   end
 
   constraints Monban::Constraints::SignedOut.new do
-    root "users#new"
+    root "users#new", as: :sign_in
     resource :session, only: [:new, :create]
     resources :users, only: [:new, :create]
   end
