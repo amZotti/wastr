67f0a3bbed2928adc40528a48e2c7fc26fc026e9
Anthony Zotti <theillbehaviored@gmail.com>
2014-07-29 16:47:52 -0400

    Account deletion destroys user votes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :categories
   
