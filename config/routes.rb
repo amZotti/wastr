a894312d54d7f189d3cf87ad5a5e46b1c95df31b
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-29 16:19:46 -0400

    fix routes formatting

--- config/routes.rb
+++ config/routes.rb
@@ -5,16 +5,19 @@ Rails.application.routes.draw do
   constraints Monban::Constraints::SignedIn.new do
     root "posts#index"
     resource :session, only: [:destroy]
+    resources :categories, only: [:new, :create, :show, :edit, :update, :destroy]
+
     resources :posts, only: [:new, :create, :index, :show] do
       resources :comments, only: [:create]
       resource :upvote, only: [:create]
       resource :downvote, only: [:create]
     end
+
     resources :comments, only: [] do
       resources :replies, only: [:create]
     end
-    resources :categories, only: [:new, :create, :show, :edit, :update, :destroy]
   end
+
   constraints Monban::Constraints::SignedOut.new do
     root "users#new", as: :sign_up
     resource :session, only: [:new, :create]
