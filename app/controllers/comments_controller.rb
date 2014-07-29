ee619398f336a14392202edf2e7bb4d45ad7c50d
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-29 14:53:38 -0400

    Make comments on posts polymorphic
    -make comments polymorphic
    -make replies controller for comments that reply to other comments
    -make new routes for comments and replies

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,8 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.create(comment_params)
-    redirect_to comment.post
+    post = Post.find(params[:post_id])
+    comment = post.replies.create(comment_params)
+    redirect_to post
   end
 
   private
@@ -9,6 +10,11 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(:comment).permit(
       :body,
-    ).merge(post_id: params[:post_id])
+      :parent_id,
+      :parent_type,
+    ).merge(
+        user_id: current_user.id,
+        post_id: params[:post_id]
+      )
   end
 end
