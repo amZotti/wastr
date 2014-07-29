032512bbc5dc76668b58fa11813ace497c0f1de1
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-29 10:42:37 -0400

    Create comment partial, migrate db, update models

---
+++ db/migrate/20140729143606_add_parent_id_and_parent_type_to_comments.rb
@@ -0,0 +1,9 @@
+class AddParentIdAndParentTypeToComments < ActiveRecord::Migration
+  def change
+    add_column :comments, :parent_id, :integer, null: false
+    add_column :comments, :parent_type, :string, null: false
+    add_index :comments, :parent_id
+    add_index :comments, :parent_type
+  end
+
+end
