8af787352565507c6b121bb000129a5c3b780536
Liane Allen <liantics.antics@gmail.com>
2014-07-30 15:27:47 -0400

    Remove unused post_categories from category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ActiveRecord::Base
   belongs_to :user
-  has_many :post_categories
   has_many :posts, through: :post_categories, source: :category
 
   validates :name, presence: true, uniqueness: true
