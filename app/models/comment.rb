bfbc52393ebce1c91b4697c7572143698d452738
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-30 09:52:34 -0400

    Add permalinks to comments partial

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,18 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  delegate :email, to: :user
   belongs_to :post
   has_many :comments, as: :parent, dependent: :destroy
-  has_many :replies,  class_name: "Comment", foreign_key: "parent_id", as: :parent
+  has_many :replies, class_name: "Comment", foreign_key: "parent_id", as: :parent
   belongs_to :parent, polymorphic: true
+
+  delegate :email, to: :user
 
   validates :user, presence: true
   validates :post, presence: true
   validates :body, presence: true
   validates :parent, presence: true
+
+  def path
+    [self.post, self]
+  end
 end
