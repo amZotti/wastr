8de6dd5db8e36789f7df065299eafaed3c8a1f2f
Liane Allen <liantics.antics@gmail.com>
2014-07-30 17:23:38 -0400

    fix post model so validations work

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+  belongs_to :category
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
   has_many :replies, as: :parent, class_name: "Comment"
