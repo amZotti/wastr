c8b4f6e5a16d2230213fd39311f38680e8f879f5
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-30 16:00:13 -0400

    Make user#show display comment history

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :categories
-  
+
   def allowed_to_modify_category?(category)
     category_owner?(category)
   end
