e0059538fe88f3dd28083f24902612b5caf3fcd0
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-30 17:11:52 -0400

    put all the belongs_to's together

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  belongs_to :parent, polymorphic: true
   has_many :comments, as: :parent, dependent: :destroy
   has_many :replies, class_name: "Comment", foreign_key: "parent_id", as: :parent
-  belongs_to :parent, polymorphic: true
 
   delegate :email, to: :user
 
