c5df7068f8fb0f4fbe1b1b21ebad25c5033fcf46
Garth Goldwater <garth.goldwater@gmail.com>
2014-07-30 10:41:07 -0400

    Make migration safer, separate delegate into own section

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
-  delegate :email, to: :user
   belongs_to :post
   has_many :comments, as: :parent, dependent: :destroy
   has_many :replies,  class_name: "Comment", foreign_key: "parent_id", as: :parent
   belongs_to :parent, polymorphic: true
+
+  delegate :email, to: :user
 
   validates :user, presence: true
   validates :post, presence: true
